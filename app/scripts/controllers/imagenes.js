9de31fed6be3513a1d9dcc4396f7cad23aa7a4c6
Thomas Loor <pipos_rgt@hotmail.com>
2017-07-17 14:34:23 +0200

    datepicker

--- app/scripts/controllers/imagenes.js
+++ app/scripts/controllers/imagenes.js
@@ -48,4 +48,36 @@ angular.module('practica2App')
     {stateOff: 'glyphicon-off'}
   ];
 
+
+
+
+
+
+  
+  // Disable date back
+  $scope.dateOptions = {
+    dateDisabled: disabled,
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  // Disable weekend selection
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+
+  $scope.popup1 = {
+    opened: false
+  };
+
+  
+
   });
