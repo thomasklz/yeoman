8779799d73a2e916d884563f74f9d9d3f199cf60
Thomas Loor <pipos_rgt@hotmail.com>
2017-07-17 18:13:31 +0200

    ventana modal

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,8 +8,15 @@
  * Controller of the practica2App
  */
 angular.module('practica2App')
-  .controller('AboutCtrl', function ($scope,Alertify) {
-  	$scope.AlertySuccess=function(){
+  .controller('AboutCtrl', function ($scope,Alertify, $uibModal) {
+  	
+    $scope.datos = [
+        {"name" : "Bottom-Dollar Marketse" ,"city" : "Tsawassen"},
+        {"name" : "Alfreds Futterkiste", "city" : "Berlin"},
+        {"name" : "Bon app", "city" : "Marseille"}
+    ];
+
+    $scope.AlertySuccess=function(){
 		 Alertify.success("Success!");
   	}
   	$scope.AlertyDanger=function(){
@@ -19,18 +26,57 @@ angular.module('practica2App')
 		 Alertify.log("Log!");
   	}
   	
-	$scope.AlertaMensaje=function(){
+	 $scope.AlertaMensaje=function(){
 		 Alertify.alert("Mensaje de alerta!");
   	}
 
   	$scope.ConfirmDialog=function(){
   	// confirm dialog
-	Alertify.confirm('Are you sure?').then(
+    	Alertify.confirm('Are you sure?').then(
         function onOk() {
         	Alertify.success("Success!");},
         function onCancel() { 
         	 Alertify.error("Error!"); }
-    )
+      )
     };
 
-  });
+    //ventana modal
+    $scope.showModal=function(){
+     $scope.newClients={};
+  
+      var modalInstance = $uibModal.open({
+        templateUrl: 'views/modal/newClient.html',
+        controller:'myModalController',
+        resolve: {
+          newClients: function() //scope del modal
+            {
+                //retorna campos del formulario
+                return $scope.newClients;
+            }
+        }
+      });
+
+      //proceso una vez cerrada la ventana modal
+      modalInstance.result.then(function () {
+           $scope.datos.push({name:$scope.newClients.newName, city:$scope.newClients.newCity});
+      });
+
+    };
+    // fin ventana modal
+})
+
+.controller('myModalController', function($scope, $uibModalInstance, newClients)
+{
+  // una vez inyectado el objeto de datos lo inicializamos 
+  $scope.newClients = newClients;
+  $scope.save = function () {
+     //recibimos el objeto de datos 
+     $uibModalInstance.close(newClients);
+  };
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+
+});
+
+
